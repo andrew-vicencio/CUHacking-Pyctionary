739d1f1ee5dc28ba665ef6208c5a9c9bf7a0e4e1
andrew-vicencio <andrew.v1124@gmail.com>
2017-03-05 02:04:56 -0500

    Add Dynamic Background Change

--- website/script/script.js
+++ website/script/script.js
@@ -6,24 +6,33 @@ var arr;
 var score;
 var lvl;
 var succ;
+var backgrond;
 
 window.onload=function() {
   c=document.getElementById('canvas');
   cc=c.getContext('2d');
+  c.width = 640;
+  c.height = 480;
+
+  background = new Image();
+  background.width = 640;
+  background.height = 480;
+  background.src ="file:///C:/Users/avice/Documents/Github/CUHacking/CUHacking-Pyctionary/website/images/maze.png";
+  // window.alert(background.src);
   hand = {x: c.width-50, y: 100};
   hs = 10;
 
-  cc.beginPath();
-  cc.moveTo(hand.x, hand.y);
   cc.lineWidth=10;
   cc.strokeStyle='white';
+  cc.beginPath();
+  cc.moveTo(hand.x, hand.y);
 
   setInterval(update, 1000/24);
   setInterval(getPoints, 1000/24);
   // c.addEventListener('mousemove', function(e) {
   //   var rect = c.getBoundingClientRect();
-  //   right.x = (e.clientX-rect.left)/(rect.right-rect.left)*c.width;
-  //   right.y = (e.clientY-rect.top)/(rect.bottom-rect.top)*c.height;
+  //   hand.x = (e.clientX-rect.left)/(rect.right-rect.left)*c.width;
+  //   hand.y = (e.clientY-rect.top)/(rect.bottom-rect.top)*c.height;
   // });
 }
 
@@ -40,13 +49,17 @@ function getPoints() {
 }
 
 function update() {
-  cc.fillStyle='black';
-  cc.fillRect(0, 0, c.width, c.height);
+  // cc.fillStyle='black';
+  // cc.fillRect(0, 0, c.width, c.height);
+  cc.drawImage(background, 0, 0);
 
-  cc.fillStyle='white';
+  cc.fillStyle='black';
   // cc.fillRect(left.x-hs/2, left.y-hs/2, hs, hs);
   cc.fillRect(hand.x-hs/2, hand.y-hs/2, hs, hs);
   cc.lineTo(hand.x, hand.y);
   cc.stroke();
+}
 
+function changeBackground() {
+  background.src="";
 }
